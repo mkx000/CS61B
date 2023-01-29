ce3e0b5ed3e76516e26263fab5e0dd0443d17405
mkx000 <3080233675@qq.com>
2023-01-29 15:08:10 +0800

    proj1a v1.1 finished

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,2 +1,124 @@
-public class ArrayDeque {
+public class ArrayDeque<T> {
+
+    private class Array {
+        private T[] arr;
+
+        public Array(int size) {
+            arr = (T[]) new Object[size];
+        }
+    }
+
+    private int actualSize;
+    private int logicalSize;
+    private int head;
+    private int tail;
+
+    private static int initialSize = 4;
+
+    private Array array;
+
+    public ArrayDeque() {
+        this.logicalSize = 0;
+        this.actualSize = initialSize;
+        this.array = new Array(this.actualSize);
+
+        this.head = 0;
+        this.tail = 1;
+    }
+
+    public void addFirst(T item){
+        if (2 * this.logicalSize >= this.actualSize)
+            this.bigger();
+
+        this.array.arr[this.head] = item;
+        int pos = (this.head - 1 + this.actualSize) % this.actualSize;
+        this.head = pos;
+        this.logicalSize++;
+    }
+
+    public void addLast(T item){
+        if (2 * this.logicalSize >= this.actualSize)
+            this.bigger();
+
+        this.array.arr[this.tail] = item;
+        int pos = (this.tail + 1) % this.actualSize;
+        this.tail = pos;
+        this.logicalSize++;
+    }
+
+    public boolean isEmpty(){
+        return this.logicalSize == 0;
+    }
+
+    public int size(){
+        return this.logicalSize;
+    }
+
+    public void printDeque(){
+        int pos = this.head + 1;
+        for (int i = 0; i < this.logicalSize; i++){
+            if (i != 0)
+                System.out.print(" ");
+            System.out.print(this.array.arr[pos]);
+            pos = (pos + 1) % this.logicalSize;
+        }
+    }
+
+    public T removeFirst(){
+        if (this.isEmpty())
+            return null;
+        if (this.actualSize >  2 * ArrayDeque.initialSize  && 2 * this.logicalSize < this.actualSize)
+            this.smaller();
+        this.logicalSize--;
+        this.head = (this.head + 1) % this.actualSize;
+        return this.array.arr[this.head];
+    }
+
+    public T removeLast(){
+        if (this.isEmpty())
+            return null;
+        if (this.actualSize >  2 * ArrayDeque.initialSize && 2 * this.logicalSize < this.actualSize)
+            this.smaller();
+        this.logicalSize--;
+        this.tail = (this.tail - 1 + this.actualSize) % this.actualSize;
+        return this.array.arr[this.tail];
+    }
+
+    public T get(int index){
+        if (index < 0 || index > this.logicalSize - 1)
+            return null;
+        int pos = (this.head + 1 + index) % this.actualSize;
+        return this.array.arr[pos];
+    }
+
+    public void bigger() {
+        int num = this.actualSize;
+        int pos = this.head + 1;
+        Array old = this.array;
+        this.array = new Array(2 * this.actualSize);
+
+        for (int i = 0; i < num; i++){
+            this.array.arr[i + 1] = old.arr[pos];
+            pos = (pos + 1) % num;
+        }
+
+        this.actualSize *= 2;
+        this.head = 0;
+        this.tail = num + 1;
+    }
+
+    public void smaller(){
+        int num = this.actualSize;
+        int pos = this.head + 1;
+        Array old = this.array;
+        this.array = new Array(this.actualSize / 2);
+
+        for (int i = 0; i < num; i++){
+            this.array.arr[i + 1] = old.arr[pos];
+            pos = (pos + 1) % num;
+        }
+        this.actualSize /= 2;
+        this.head = 0;
+        this.tail = num + 1;
+    }
 }
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,2 +1,123 @@
-public class LinkedListDeque {
+public class LinkedListDeque<T> {
+
+    public class Node {
+        public T item;
+        public Node prev;
+        public Node next;
+
+        public Node(T item) {
+            this.item = item;
+            this.prev = null;
+            this.next = null;
+        }
+
+        public Node(){
+            this.prev = null;
+            this.next = null;
+        }
+    }
+
+
+    private Node sentinel;
+    private int dqsize;
+
+    public LinkedListDeque(){
+        Node dummyHead = new Node();
+        dummyHead.prev = dummyHead;
+        dummyHead.next = dummyHead;
+        this.sentinel = dummyHead;
+        this.dqsize = 0;
+    }
+
+    public void addFirst(T item) {
+        Node node = new Node(item);
+
+        this.sentinel.next.prev = node;
+        node.next = this.sentinel.next;
+
+        this.sentinel.next = node;
+        node.prev = this.sentinel;
+
+        this.dqsize++;
+    }
+
+    public void addLast(T item)
+    {
+        Node node = new Node(item);
+
+        node.prev = this.sentinel.prev;
+        this.sentinel.prev.next = node;
+
+        node.next = this.sentinel;
+        this.sentinel.prev = node;
+
+        this.dqsize++;
+    }
+
+    public boolean isEmpty() {
+        return this.dqsize == 0;
+    }
+
+    public int size(){
+        return this.dqsize;
+    }
+
+    public void printDeque() {
+        Node node = this.sentinel.next;
+
+        boolean first  = true;
+        while (node != this.sentinel){
+            if (first)
+                first = false;
+            else
+                System.out.print(" ");
+            System.out.print(node.item);
+            node = node.next;
+        }
+    }
+
+    public T removeFirst(){
+        if (isEmpty())
+            return null;
+        T item = this.sentinel.next.item;
+        this.sentinel.next.next.prev = this.sentinel;
+        this.sentinel.next = this.sentinel.next.next;
+        this.dqsize--;
+        return item;
+    }
+
+    public T removeLast(){
+        if (isEmpty())
+            return null;
+        T item = this.sentinel.prev.item;
+        this.sentinel.prev.prev.next = this.sentinel;
+        this.sentinel.prev = this.sentinel.prev.prev;
+        this.dqsize--;
+        return  item;
+    }
+
+    public T get(int index){
+        if (index > dqsize - 1 || index < 0)
+            return null;
+        Node node = this.sentinel.next;
+        while (index != 0){
+            node = node.next;
+            index--;
+        }
+        return node.item;
+    }
+
+
+
+    public T getRecursive(int index){
+        if (index > dqsize - 1 || index < 0)
+            return null;
+        return getRecursive(index, this.sentinel.next);
+    }
+
+    private T getRecursive(int index, Node node){
+        if (index == 0)
+            return node.item;
+        return getRecursive(index - 1, node.next);
+    }
 }
